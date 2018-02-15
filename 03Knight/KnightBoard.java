ff9dddfbaacce80e23cd78e068a4d7246d2aff78
shuxinwu <swu10@stuy.edu>
2018-02-15 15:26:52 -0500

    count

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,14 +1,19 @@
 public class KnightBoard{
     private int[][] board;
+    private int[][] count;
+    private int r;
+    private int c;
     
     public KnightBoard(int startingRows, int startingCols){
 	if (startingRows < 0 || startingCols < 0){
 	    throw new IllegalArgumentException();
 	}
 	board = new int[startingRows][startingCols];
+	count = new int[startingRows][startingCols];
 	for (int a = 0; a < startingRows; a++){
 	    for (int b = 0; b < startingCols; b++){
 	        board[a][b] = 0;
+		count[a][b] = 0;
 	    }
 	}
     }
@@ -20,36 +25,56 @@ public class KnightBoard{
 		if (board[a][b] == 0){
 		    ans += "_";
 		}
+		else{
+		    if (board[a][b] < 10){
+			ans += " ";
+		    }
+		    ans += board[a][b];
+		}
 		ans += " ";
 	    }
 	    ans += "\n";
 	}
 	return ans;
     }
-
-    public void move(int r, int c, int x, int y){
-	r += x;
-	r += y;
-	
-    }
-
     
     public boolean solve(int startingRow, int startingCol){
+	r = startingRow;
+	c = startingCol;
+	count[r][c] = 1;
+	/*
+	System.out.println(startingRow < 0);
+	System.out.println(startingCol < 0);
+	System.out.println(startingCol > board[0].length);
+	System.out.println(startingRow > board.length);
+	*/
+	if (startingRow < 0 || startingCol < 0 ||
+	    startingCol > board[0].length || startingRow > board.length){
+       	    throw new IllegalArgumentException();
+	}
 	for (int a = 0; a < board.length; a++){
 	    for (int b = 0; b < board[0].length; b++){
-		if (board[a][b] == 0){
+		if (board[a][b] != 0){
 		  throw new IllegalStateException();
 		}
-	if (startingRow < 0 || startingCol < 0 || startingCol > || startingRow > ){
-	    throw new IllegalArgumentException();
+	    }
 	}
-	
+	return solveH(startingRow, startingCol, 0);
     }
 
-    /*
-    public int countSolutions(int startingRow, int startingCol){}
+    private boolean solveH(int row, int col, int level){
+    // level is the # of the knight
+	if (level == board.length * board[0].length){
+	    return true;
+	}	
+	return false;
+    }
 
-    private boolean solveH(int row, int col, int level){}
-    */
+    public void move(int x, int y){
+	r += x;
+	c += y;	
+    }
+ 
+    // public int countSolutions(int startingRow, int startingCol){}
 	
 }
