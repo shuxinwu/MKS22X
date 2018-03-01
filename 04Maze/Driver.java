6b292ffb376fa74c5aded942a240a52a21964cc8
shuxinwu <swu10@stuy.edu>
2018-03-01 15:14:03 -0500

    driver

---
+++ 04Maze/Driver.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver{
+
+    public static void main(String[]args){
+	try{
+	Maze f;
+        f = new Maze("data1.dat");//true animates the maze.
+	//   f.setAnimate(true);
+	//     f.solve();
+
+	System.out.println(f);
+	System.out.println(f.getMaze());
+	}
+	catch(FileNotFoundException e){
+	}
+	//    System.out.println(f);
+    }
+}
