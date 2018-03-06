b475ba519f902d298e28556d6438f16e362aa383
shuxinwu <swu10@stuy.edu>
2018-03-05 23:42:25 -0500

    setup

---
+++ 05USACO/USACO.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class USACO{
+  public static int bronze(String filename){
+    // read in file --> instructions, array
+    private int[] first;
+    private int[][] squares;
+
+    File text = new File(filename);
+    Scanner inf = new Scanner(text);
+    int i = 0;
+    while(inf.hasNextLine()){
+      if (i == 0){
+
+      }
+
+      squares = new int[first[0]][first[1]];
+      }
+      i++;
+    }
+
+    // follow through w stomps, recalc nums
+
+    // convert to inches
+
+    // return
+
+  }
+
+  // public static int silver(String filename);
+}
