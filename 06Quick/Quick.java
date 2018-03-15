53e9d8e4145ae7592f4e914faa24a70683bb9616
shuxinwu <swu10@stuy.edu>
2018-03-14 21:06:26 -0400

    swap

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -16,102 +16,30 @@ public class Quick{
 	int small = start + 1;
 	int large = end;
 
-	// values of nums
-	int s;
-	int l;
-
 	System.out.println(Arrays.toString(data));
 	
-	//	while (small < large){
-	for (int i = 0; small <= large; i++){
-	    s = data[small];
-	    l = data[large];
-
-	    if (s < pivot){
+	while (small <= large){
+	    if (data[small] < pivot){
 		small++;
-		System.out.println(1);
-		System.out.println("i: " + i);
-		System.out.println("s: " + s);
-		System.out.println("l: " + l);
-		System.out.println(data[small]);
-		System.out.println(data[large]);
-		System.out.println("small: " + small);
-		System.out.println("large: " + large);
-		System.out.println(Arrays.toString(data));
-	    }
-	    
-	    if (l > pivot){
-		large--;
-		System.out.println(2);
-		System.out.println("i: " + i);
-	        System.out.println("s: " + s);
-		System.out.println("l: " + l);
-		System.out.println(data[small]);
-		System.out.println(data[large]);
-		System.out.println("small: " + small);
-		System.out.println("large: " + large);
-		System.out.println(Arrays.toString(data));
-	    }	    
-	   	    
+	    }	    	   	    
 	    else{
-		s = data[small];
-		l = data[large];
-		data[small] = l;
-		data[large] = s;
-		small++;
 		large--;
-		System.out.println(3);
-		System.out.println("i: " + i);
-	        System.out.println("s: " + s);
-		System.out.println("l: " + l);
-		System.out.println(data[small]);
-		System.out.println(data[large]);
-		System.out.println("small: " + small);
-		System.out.println("large: " + large);
-		System.out.println(Arrays.toString(data));
 	    }
 	
 	    //	    System.out.println(Arrays.toString(data));
 	}
 	
-	/*
-	String m = "";
-	for (int i = 0; i < data.length; i++){
-	    m += data[i];
-	    m += " ";
-	}
-	System.out.println(m);
-	*/
-
-	for (int i = start; i < large; i++){
-	    int a = data[i + 1];
-	    data[i] = a;
-	}
 	data[large] = pivot;
-
-	System.out.println(Arrays.toString(data));
-	
-	try{
-	    if (start != large && data[large] < data[large - 1]){
-		int a = data[large - 1];
-		data[large] = a;
-		data[large - 1] = pivot;
-		large--;
-	    }
-	    if (data[large] > data[large + 1]){
-		int a = data[large + 1];
-		data[large] = a;
-		data[large + 1] = pivot;
-		large++;
-	    }
-	}
-	catch (ArrayIndexOutOfBoundsException e){
-	}
-	
-	System.out.println(Arrays.toString(data));
 	return large;
     }
 
+    public static void swap(int[] data, int a, int b){
+	int x = data[a];
+	int y = data[b];
+	data[a] = y;
+	data[b] = x;
+    }
+    
     /*
     public static int quickSelect(int[] data, int k){
 	int i = partition(data, 0, data.length - 1);
