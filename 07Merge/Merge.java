db9813dedfe56062e1a58a9d71b1a6acb69ee879
shuxinwu <swu10@stuy.edu>
2018-03-19 22:10:41 -0400

    setup

---
+++ 07Merge/Merge.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Merge{
+
+    public static void mergeSort(int[] data){};
+
+    public static void merge(int[] a, int[] b){};
+
+    public static void main(String[] args){};
+}
+    
