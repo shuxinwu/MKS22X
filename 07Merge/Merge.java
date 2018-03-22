0e497930375904e8768dec35ad4cb6c57efe6fe3
shuxinwu <swu10@stuy.edu>
2018-03-22 14:24:01 -0400

    merge

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -4,7 +4,7 @@ public class Merge{
 
     // sort the array from least to greatest value. This is a wrapper function that calls msort()
     public static void mergesort(int[]data){
-	int[] temp = int[data.length];
+	int[] temp = new int[data.length];
 	msort(data, temp, 0, data.length - 1);
     }
 
@@ -21,7 +21,7 @@ public class Merge{
 	    //  call msort() on each half, but switch the temp and data!
 	    msort(temp, data, lo, mid);
 	    msort(temp, data, mid + 1, hi);
-	    //  call merge...
+	    //  call merge
 	    merge(data, temp, lo, mid, hi);
 	}
 	
@@ -30,10 +30,25 @@ public class Merge{
 
     // copy from temp back to data
     public static void merge(int[] data, int[] temp, int lo, int mid, int hi){
+	int l = lo;
 	int m = mid + 1;
-	for (int i = lo; i < hi; i++){
-	    
+	int a = temp[lo];
+	int b = temp[m];
+	for (int i = l ; i < hi; i++){
+	    //	    a = temp[lo];
+	    //	    b = temp[m];
+	    if (a < b){
+		data[i] = a;
+		lo++;
+		a = temp[lo];
+	    }
+	    else{
+		data[i] = b;
+		m++;
+		b = temp[m];
+	    }
 	}
+	
     }
 
     public static void main(String[] args){};
