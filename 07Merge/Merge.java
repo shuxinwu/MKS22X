74e8a872c36baeebcb9545612d0ffed9de884012
shuxinwu <swu10@stuy.edu>
2018-03-25 23:08:30 -0400

    mergesort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -4,19 +4,22 @@ public class Merge{
 
     // sort the array from least to greatest value. This is a wrapper function that calls msort()
     public static void mergesort(int[] data){
-	int[] temp = new int[data.length];
-	msort(data, temp, 0, data.length - 1);
+	int z = data.length;
+	int[] temp = new int[z];
+      	msort(data, temp, 0, z - 1);
     }
 
     // Suggestions - have your msort take both the data and temp arrays. Pre-allocate a temp array in your mergesort method
-    private static void msort(int[]data, int[]temp, int lo, int hi){	
-	if (lo <= hi){
+    private static void msort(int[]data, int[]temp, int lo, int hi){
+	//	System.out.println("lo: " + lo);
+	//	System.out.println("hi: " + hi);
+	if (lo < hi){
 	    //   copy   data[ lo ] through data[hi]    into    temp[lo] through temp[hi]
 	    for (int i = lo; i <= hi; i++){
 		int a = data[i];
 		temp[i] = a;
 	    }
-	    System.out.println("temp: " + Arrays.toString(temp));
+	    //	    System.out.println("temp: " + Arrays.toString(temp));
 	   
 	    // get mid
 	    int mid = (lo + hi) / 2;
@@ -24,10 +27,8 @@ public class Merge{
 	    msort(temp, data, lo, mid);
 	    msort(temp, data, mid + 1, hi);
 	    //  call merge
-	    merge(data, temp, lo, mid, hi);
-	   
+	    merge(data, temp, lo, mid, hi);	   
 	}
-	
     }
 
     // copy from temp back to data
@@ -49,12 +50,16 @@ public class Merge{
 	    if (a < b){
 		data[i] = a;
 		lo++;
-		a = temp[lo];
+		if (lo < temp.length){
+		    a = temp[lo];
+		}
 	    }
 	    else{
 		data[i] = b;
 		m++;
-		b = temp[m];
+	        if (m < temp.length){
+		    b = temp[m];
+		}
 	    }
 	    i++;
 	    //	    System.out.println("data:" + Arrays.toString(data));
@@ -88,8 +93,9 @@ public class Merge{
     }
 
     public static void main(String[] args){
-       	int[] ary = {1, 3, 5, 7, 0, 4, 8, 14};
-       	int[] data = {14, 4, 1, 7, 0, 8, 5, 3};
+	/*
+       	int[] ary = { 3, 5, 7, 0, 4, 8, 14};
+       	int[] data = {14, 4, 7, 0, 8, 5, 3};
 	//	int[] ary = {3, 4, 1, 0, 6, 6, 4, 88, 22, 100};
 	//	int[] ary = {0, 1, 2, 1, 2, 1, 1, 0};
        	System.out.println("ary:" + Arrays.toString(ary));
@@ -99,6 +105,7 @@ public class Merge{
         mergesort(data);
 	System.out.println("ary:" + Arrays.toString(ary));
 	System.out.println("data:" + Arrays.toString(data));
+	*/
     }
 
 }
