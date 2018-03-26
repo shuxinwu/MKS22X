5a6220ea049a48f30ccdf34002d5ce81cfa6db39
shuxinwu <swu10@stuy.edu>
2018-03-25 22:19:37 -0400

    copy ary error

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -3,19 +3,21 @@ import java.util.*;
 public class Merge{
 
     // sort the array from least to greatest value. This is a wrapper function that calls msort()
-    public static void mergesort(int[]data){
+    public static void mergesort(int[] data){
 	int[] temp = new int[data.length];
 	msort(data, temp, 0, data.length - 1);
     }
 
     // Suggestions - have your msort take both the data and temp arrays. Pre-allocate a temp array in your mergesort method
     private static void msort(int[]data, int[]temp, int lo, int hi){	
-	if (lo >= hi){
+	if (lo <= hi){
 	    //   copy   data[ lo ] through data[hi]    into    temp[lo] through temp[hi]
 	    for (int i = lo; i < hi; i++){
 		int a = data[i];
 		temp[i] = a;
 	    }
+	    System.out.println("temp" + Arrays.toString(temp));
+	    /*
 	    // get mid
 	    int mid = (lo + hi) / 2;
 	    //  call msort() on each half, but switch the temp and data!
@@ -23,6 +25,7 @@ public class Merge{
 	    msort(temp, data, mid + 1, hi);
 	    //  call merge
 	    merge(data, temp, lo, mid, hi);
+	    */
 	}
 	
     }
@@ -37,11 +40,12 @@ public class Merge{
 	int a = temp[lo];
 	int b = temp[m];
 	int i = l;
-	System.out.println("mid: " + mid);
+	//	System.out.println("mid: " + mid);
+	
 	// comparison
 	while (lo <= mid && m <= hi){
-	    System.out.println(a);
-	    System.out.println(b);
+	    //	    System.out.println(a);
+	    //	    System.out.println(b);
 	    if (a < b){
 		data[i] = a;
 		lo++;
@@ -53,14 +57,14 @@ public class Merge{
 		b = temp[m];
 	    }
 	    i++;
-	    System.out.println("data:" + Arrays.toString(data));
+	    //	    System.out.println("data:" + Arrays.toString(data));
 	}
 	if (lo > mid){
 	    while (i <= hi){
-		System.out.println("i: " + i);;
-		System.out.println("b: " + b);
+		//		System.out.println("i: " + i);;
+		//		System.out.println("b: " + b);
 		data[i] = b;
-		System.out.println(Arrays.toString(data));
+		//		System.out.println(Arrays.toString(data));
 		m++;
 		if (m < temp.length){
 		    b = temp[m];
@@ -70,8 +74,8 @@ public class Merge{
 	}
 	if (m > hi){
 	    while (i <= hi){
-		System.out.println("i: " + i);
-		System.out.println("a: " + a);
+		//		System.out.println("i: " + i);
+		//		System.out.println("a: " + a);
 		data[i] = a;
 		lo++;
 		if (lo < temp.length){
@@ -91,8 +95,8 @@ public class Merge{
        	System.out.println("ary:" + Arrays.toString(ary));
 	System.out.println("data:" + Arrays.toString(data));
       	//	System.out.println(ary.length);
-        merge(data, ary, 0, 3, data.length - 1);
-	//      mergesort(ary);
+	//        merge(data, ary, 0, 3, data.length - 1);
+        mergesort(data);
 	System.out.println("ary:" + Arrays.toString(ary));
 	System.out.println("data:" + Arrays.toString(data));
     }
