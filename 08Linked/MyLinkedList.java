1d0358054174965138f76ce4457f3344d3ff644e
shuxinwu <swu10@stuy.edu>
2018-03-27 22:23:49 -0400

    setup

---
+++ 08Linked/MyLinkedList.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MyLinkedList{
+    Node first, last;
+    int length;
+
+    public MyLinkedList(){
+	List l = new LinkedList();
+    }
+
+    /*
+    public String toString(){};
+
+    public int get(n){};
+
+    public void set(int index, int value){};
+
+    public int size(){};
+
+    public add(int index, int value){};
+
+    public remove(int index){};
+    */
+}
