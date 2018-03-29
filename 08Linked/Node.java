d4b2d4a791fc33cda20a6b46b0fb679a37ea41c5
shuxinwu <swu10@stuy.edu>
2018-03-28 23:44:36 -0400

    toString loop

--- 08Linked/Node.java
+++ 08Linked/Node.java
@@ -19,8 +19,9 @@ public class Node{
 	return prev;
     }
 
-    public void setPrev(Node a){
-	prev = a;
+    public void setPrev(int a){
+	Node b = new Node(a);
+	prev = b;
     }
 
     public int getValue(){
@@ -32,17 +33,22 @@ public class Node{
     }
 
     public String toString(){
-	String s = "";
-	while (next != null){
-	    s += getValue();
-	    s += " ";
+	String s = " ";
+	Node c = next;
+	//	int test = 0;
+	while (c.getNext() != null){
+	    s += c.getValue();
+	    s += ", ";
+	    c = next;
+	    //	    test++;
+	    //	    System.out.println(test);
 	}
 	return s;
     }
 
     public static void main(String[] args){
 	Node a = new Node(4);
-	System.out.println(a);
+	//	System.out.println(a);
 	a.setNext(14);
 	System.out.println(a);
     }
