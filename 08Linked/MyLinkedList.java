3fea81cc6ef9a70453d1909fa28ee4ac32edfd6e
shuxinwu <swu10@stuy.edu>
2018-04-11 01:22:56 -0400

    npe add

--- 08Linked/MyLinkedList.java
+++ 08Linked/MyLinkedList.java
@@ -63,23 +63,96 @@ public class MyLinkedList{
 	current.setValue(value);
 	return i;
     }
+
+    public Integer indexOf(Integer value){
+	Node current = first;
+	for (Integer i = 0; i < length; i++){
+	    if (current.getValue() == value){
+		return i;
+	    }
+	    current = current.getNext();
+	}
+	return -1;
+    }
+
+    // not added, or not printing
+    public void add(int index, Integer value){ // exceptions
+	Node current = first;
+	int a = 0, b = 0;
+	int i = 0;
+        while (i <= length){
+	    if (i == index && i != length){
+		a = current.getValue();
+		current.setValue(value);
+		if (index == 0){
+		    first = current;
+		}
+	    }
+	    if (i > index && i < length){
+		b = current.getValue();
+		current.setValue(a);
+		a = b;
+
+	    }
+	    if (i == length){
+		Node f = new Node(a);
+		current = f;
+	    }
+	    if (current != null){
+		current = current.getNext();	    
+	    }
+	    i++;
+	}
+	last = current;
+	length++;
+    }
+    
     //    private Node getNode(int index){};
     
-    /*
 
-    public void clear();
+    public void clear(){
+	Node current = first;
+	Node next = current.getNext();
+	for (int i = 0; i < length; i++){
+	    next = current.getNext();
+	    current = null;
+	    next = current;
+	}
+    }
 
-    public int indexOf(Integer value);
+    public boolean remove(Integer value){
+	int i = 0;
+	Node current = first;
+	while (i < length){
+	    if (current.getValue() == value){
+		current.setValue(current.getNext().getValue());
+	    }
+	    current = current.getNext();
+	    i++;
+	}
+	length--;
+	return true;
+    }
 
-    public void add(int index, Integer value); // exceptions
+    public int remove(int index){
+	int i = 0;
+	int v = 0;
+	Node current = first;
+	while (i < length){
+	    if (i == index){
+		v = current.getValue();
+		current.setValue(current.getNext().getValue());
+	    }
 
-    public boolean remove(Integer value);
-
-    public remove(int index){};
+	    current = current.getNext();
+	    i++;
+	}
+	length--;
+	return v;
+    }
 //IndexOutOfBoundsException - if the index is out of range (index < 0 || index >= size())
 
 
-    */
 
     public static void main(String[] args){
 	MyLinkedList a = new MyLinkedList();
@@ -91,6 +164,9 @@ public class MyLinkedList{
 	System.out.println(a);
 	System.out.println(a.get(4));
 	a.set(3, 1);
+	System.out.println(a);
+	System.out.println(a.indexOf(1));
+	a.add(3, 2);
 	System.out.println(a);
     }
 }
