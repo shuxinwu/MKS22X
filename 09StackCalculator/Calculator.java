dab9848ad1175c4a5c79245315156400eaf2affa
shuxinwu <swu10@stuy.edu>
2018-04-18 15:33:45 -0400

    substring error

---
+++ 09StackCalculator/Calculator.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Calculator{
+
+    public static double eval(String exp){
+	Stack<Double> stack = new Stack<Double>();
+	int b = 0;
+	for (int i = 0; i < exp.length(); i++){
+	    // b tracks start of num to look at
+	   
+	    if (exp.charAt(i) == '+'){
+	       	stack.push(stack.pop() + stack.pop());
+		b = i + 1;
+	    }
+
+	    if (exp.charAt(i) == '-'){
+	       	stack.push(-1 * (stack.pop() - stack.pop()));
+		b = i + 1;
+	    }
+
+	    if (exp.charAt(i) == '*'){
+	       	stack.push(stack.pop() * stack.pop());
+		b = i + 1;
+	    }
+
+	    if (exp.charAt(i) == '/'){
+	        double x = stack.pop();
+		double y = stack.pop();
+		stack.push(y / x);
+		b = i + 1;
+	    }
+
+	    if (exp.charAt(i) == '%'){
+		double x = stack.pop();
+		double y = stack.pop();
+		stack.push(y % x);
+		b = i + 1;
+	    }
+	    	    
+	    // sep by space
+	    if (exp.charAt(i) == ' '){
+		System.out.println("i: " + i);
+	    System.out.println("b: " + b);
+		System.out.println("sub: " + exp.substring(b, i));
+		
+		if (exp.substring(b, i).indexOf('.') == -1){
+		    stack.push(Integer.parseInt(exp.substring(b, i)) + 0.0);
+		}
+		else{
+		    stack.push(Double.parseDouble(exp.substring(b, i)));
+		}
+		b = i + 1;
+	    }
+	    
+	    System.out.println(stack);
+	}
+	
+	return stack.pop();
+    }
+
+    public static void main(String[] args){
+       	System.out.println(eval("11 3 - 4 + 2.5 *"));
+
+	/*
+	eval("10 2.0 +") is 12.0
+	    eval("11 3 - 4 + 2.5 *") is 30.0
+	    eval("8 2 + 99 9 - * 2 + 9 -") is 893.0
+	*/
+    }
+}
