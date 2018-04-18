cb3b3f9989c081dc35cec647bdb0144552002b19
shuxinwu <swu10@stuy.edu>
2018-04-18 01:03:22 -0400

    b error

--- 08Linked/eval.java
+++ 08Linked/eval.java
@@ -6,47 +6,45 @@ public class eval{
     public static double eval(String exp){
 	Stack<Double> stack = new Stack<Double>();
 	for (int i = 0; i < exp.length(); i++){
+	    // b tracks start of num to look at
 	    int b = 0;
 
 	    if (exp.charAt(i) == '+'){
 	       	stack.push(stack.pop() + stack.pop());
-		b = i + 1;
 	    }
 
 	    if (exp.charAt(i) == '-'){
 	       	stack.push(-1 * (stack.pop() - stack.pop()));
-		b = i + 1;
 	    }
 
 	    if (exp.charAt(i) == '*'){
 	       	stack.push(stack.pop() * stack.pop());
-		b = i + 1;
 	    }
 
 	    if (exp.charAt(i) == '/'){
 	        double x = stack.pop();
 		double y = stack.pop();
 		stack.push(y / x);
-		b = i + 1;
 	    }
 
 	    if (exp.charAt(i) == '%'){
 		double x = stack.pop();
 		double y = stack.pop();
 		stack.push(y % x);
-		b = i + 1;
 	    }
 	    
 	    // sep by space
 	    if (exp.charAt(i) == ' '){
-		try{
+		System.out.println(exp.substring(b, i) + "sub");
+		if (exp.substring(b, i).indexOf('.') == -1){
 		    stack.push(Integer.parseInt(exp.substring(b, i)) + 0.0);
 		}
-		catch(NumberFormatException e){
+		else{
 		    stack.push(Double.parseDouble(exp.substring(b, i)));
 		}
-		b = i + 1;
 	    }
+
+	    b = i + 1;
 	    System.out.println(i);
 	    System.out.println(b);
 	    System.out.println(stack);
@@ -56,6 +54,7 @@ public class eval{
     }
 
     public static void main(String[] args){
+	System.out.println("babies".indexOf('p'));
        	System.out.println(eval("11 3 - 4 + 2.5 *"));
 
 	/*
