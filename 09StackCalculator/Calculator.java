82c450e68adfc91cafcb0e596e254f16d5c6ec4a
shuxinwu <swu10@stuy.edu>
2018-04-18 20:39:18 -0400

    test

--- 09StackCalculator/Calculator.java
+++ 09StackCalculator/Calculator.java
@@ -5,9 +5,9 @@ public class Calculator{
 
     public static double eval(String exp){
 	Stack<Double> stack = new Stack<Double>();
+	// b tracks start of num to look at
 	int b = 0;
 	for (int i = 0; i < exp.length(); i++){
-	    // b tracks start of num to look at
 	   
 	    if (exp.charAt(i) == '+'){
 	       	stack.push(stack.pop() + stack.pop());
@@ -37,35 +37,39 @@ public class Calculator{
 		stack.push(y % x);
 		b = i + 1;
 	    }
-	    	    
+
+	    
 	    // sep by space
 	    if (exp.charAt(i) == ' '){
+		/*
 		System.out.println("i: " + i);
-	    System.out.println("b: " + b);
+		System.out.println("b: " + b);
 		System.out.println("sub: " + exp.substring(b, i));
-		
-		if (exp.substring(b, i).indexOf('.') == -1){
-		    stack.push(Integer.parseInt(exp.substring(b, i)) + 0.0);
+		*/
+		if (i == b){
+		    b++;
 		}
 		else{
-		    stack.push(Double.parseDouble(exp.substring(b, i)));
+		    if (exp.substring(b, i).indexOf('.') == -1){
+			stack.push(Integer.parseInt(exp.substring(b, i)) + 0.0);
+		    }
+		    else{
+			stack.push(Double.parseDouble(exp.substring(b, i)));
+		    }
+		    b = i + 1;
 		}
-		b = i + 1;
-	    }
-	    
-	    System.out.println(stack);
-	}
-	
+	    }	    
+	    //	    System.out.println(stack);
+	}	
 	return stack.pop();
     }
 
     public static void main(String[] args){
-       	System.out.println(eval("11 3 - 4 + 2.5 *"));
-
+	//  	System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
 	/*
 	eval("10 2.0 +") is 12.0
-	    eval("11 3 - 4 + 2.5 *") is 30.0
-	    eval("8 2 + 99 9 - * 2 + 9 -") is 893.0
+	eval("11 3 - 4 + 2.5 *") is 30.0
+	eval("8 2 + 99 9 - * 2 + 9 -") is 893.0
 	*/
     }
 }
