41273c391eda0ec4ffee92d3439aa2254f1747f0
shuxinwu <swu10@stuy.edu>
2018-04-29 21:17:05 -0400

    setup

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,19 @@
+public class MyHeap{
+    private String[] data;
+    
+    public MyHeap(){};
+
+    // true: construct empty max heap, false: construct empty min heap
+    public MyHeap(boolean a){
+	
+    }
+
+    public void add(String S){};
+
+    public String remove(){};
+
+    public String peek(){};
+
+    public int size(){};
+
+}
