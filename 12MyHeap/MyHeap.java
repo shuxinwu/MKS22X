9d702e36a2a2a1b61ea1e9617953c57ca50fab44
shuxinwu <swu10@stuy.edu>
2018-04-30 22:01:40 -0400

    peek

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -4,26 +4,43 @@ import java.util.*;
 public class MyHeap{
     private String[] data;
     private int size;
+    private boolean isMax;
     
     public MyHeap(){
 	data = new String[10];
 	size = 0;
+	isMax = true;
     }
 
     //  true: construct empty max heap, false: construct empty min heap
-    //  public MyHeap(boolean a){};
+    public MyHeap(boolean a){
+	data = new String[10];
+	size = 0;
+	isMax = a;
+    }
 
     public void add(String S){ 
 	int a = (size - 1) / 2;
 	int b = size;
 	String temp;
 	data[b] = S;
-	while (data[b].compareTo(data[a]) > 0){
-	    temp = data[a];
-	    data[a] = data[b];
-	    data[b] = temp;
-	    b = a;
-	    a = (a - 1) / 2;
+	if (isMax){
+	    while (data[b].compareTo(data[a]) > 0){
+		temp = data[a];
+		data[a] = data[b];
+		data[b] = temp;
+		b = a;
+		a = (a - 1) / 2;
+	    }
+	}
+	else{
+	    while (data[b].compareTo(data[a]) < 0){
+		temp = data[a];
+		data[a] = data[b];
+		data[b] = temp;
+		b = a;
+		a = (a - 1) / 2;
+	    }
 	}
 	size++;
     }
@@ -35,7 +52,9 @@ public class MyHeap{
 	return old;
     }
 
-    //  public String peek(){};
+    public String peek(){
+	return data[0];
+    }
 
     public int size(){
 	return size;
@@ -60,5 +79,6 @@ public class MyHeap{
 	a.remove();
 	System.out.println(a);
 	System.out.println(a.size());
+	System.out.println(a.peek());
     }
 }
