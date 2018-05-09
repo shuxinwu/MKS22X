a3b3585775938b2bd13f59ea348b56a37b542fbc
shuxinwu <swu10@stuy.edu>
2018-05-08 22:58:34 -0400

    constructor and toString

--- 14Frontier/Maze.java
+++ 14Frontier/Maze.java
@@ -1,9 +1,37 @@
+import java.util.*;
+
 public class Maze{
     private Location start,end;
     private char[][] board;
+    int[][] moves = { {0,1}, {1,0}, {-1,0}, {0,-1} };
 
     public Maze(String mazeText){
-	
+	Scanner inf = new Scanner(mazeText);
+	int row = 0;
+	int col = 0;
+	while(inf.hasNextLine()){
+	    row++;
+	    if (row == 1){
+		String l = inf.nextLine();
+		col = l.length();
+	    }
+	}
+	board = new char[row][col];
+	Scanner in = new Scanner(mazeText);
+	for (int i = 0; i < row; i++){
+	    String line = in.nextLine();
+	    board[i] = line.toCharArray();
+	}
+	for (int a = 0; a < row; a++){
+	    for (int b = 0; b < col; b++){
+		if (board[a][b] == 'S'){
+		    start = new Location(a, b, null);
+		}
+		if (board[a][b] == 'E'){
+		    end = new Location(a, b, null);
+		}
+	    }
+	}
     }
 
     // '#' - wall 
@@ -14,8 +42,17 @@ public class Maze{
     // '@' - part of solution
     // 'S' - starting space (do not replace this)
     public String toString(){
+	String s = "";
+	for (int a = 0; a < board.length; a++){
+	    for (int b = 0; b < board[0].length; b++){
+		s += board[a][b];
+	    }
+	    s += "\n";
+	}
+	return s;
     }
-   
+
+    /*
     // Work on this method as a group! 
     public String toStringColor(){
     }
@@ -24,6 +61,7 @@ public class Maze{
     // adjacent to n and  not visited
     // all the Locations in this list should have their previous set to n.
     public Location[] getNeighbors(Location n){
+	
 	return null;
     }
 
@@ -35,6 +73,6 @@ public class Maze{
 	return null;
     }
 
-
+    */
 
 }
